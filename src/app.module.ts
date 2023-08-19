99cc0a6bd7656b513aaa0161189d452743ce1c21
tinp <nhattin55@gmail.com>
2023-08-19 12:24:26 -0700

    created schema, service class-model injection, and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_URI)],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_URI), ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
