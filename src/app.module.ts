abc3969f8b839d650c546691366bea94d792d2cc
taylorpapke <taylorpapke@gmail.com>
2023-08-24 12:23:55 -0700

    created user controller, services and model modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_URI), ProductsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(), 
+    MongooseModule.forRoot(process.env.MONGODB_URI), 
+    ProductsModule, 
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
