e2ebc1e12e416ba983cb875b02f2858fd240dc68
CourtneyHoppus <cobaltab@gmail.com>
2023-10-30 13:18:58 -0700

    imported PassportMod and registered JwtMod to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 
 @Module({
@@ -12,7 +14,9 @@ import { UserModule } from './user/user.module';
     MongooseModule.forRoot(process.env.MONGODB_URI),
     ProductsModule,
     AuthModule,
-    UserModule
+    UserModule,
+    PassportModule,
+    JwtModule.register({ secret: 'secret', signOptions: { expiresIn: '4h' } })
   ],
   controllers: [],
   providers: [],
