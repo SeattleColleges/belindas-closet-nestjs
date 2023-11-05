d79635fc0dc683181a280d7908855377197a2da0
CourtneyHoppus <cobaltab@gmail.com>
2023-11-05 11:35:01 -0800

    created service alias, provided this to test

--- src/products/controllers/products/products.controller.spec.ts
+++ src/products/controllers/products/products.controller.spec.ts
@@ -1,18 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsController } from './products.controller';
+import { ProductsService } from 'src/products/services/products/products.service';
+
 
 describe('ProductsController', () => {
   let controller: ProductsController;
+  let service: ProductsService;
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
+      providers: [
+        {
+          provide: 'PRODUCTS_SERVICE',
+          useValue: { }
+        }
+      ],
     }).compile();
 
     controller = module.get<ProductsController>(ProductsController);
+    service = module.get<ProductsService>('PRODUCTS_SERVICE');
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+  
+  it('service should be defined', () => {
+    expect(service).toBeDefined();
+  })
 });
