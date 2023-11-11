ebeb36af8f1eb9ba470baa9eacfc01e244dbc30e
CourtneyHoppus <cobaltab@gmail.com>
2023-11-11 14:05:45 -0800

    added service tests

--- src/user/services/user/user.service.spec.ts
+++ src/user/services/user/user.service.spec.ts
@@ -1,15 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { Model } from 'mongoose';
-import { Role, User } from '../../schemas/user.schema';
+import { User } from '../../schemas/user.schema';
 import { getModelToken } from '@nestjs/mongoose';
+import { HttpException } from '@nestjs/common';
 
 describe('UserService', () => {
   let service: UserService;
   let model: Model<User>;
 
   let mockUserModel = {
-    create: jest.fn()
+    find: jest.fn(),
+    findById: jest.fn(),
+    findOne: jest.fn(),
+    findByIdAndUpdate: jest.fn()
+  }
+
+  let mockUser = {
+    id: 'user.id',
+    firstName: 'user.firstName',
+    lastName: 'user.lastName',
+    email: 'user@email.com',
+    role: 'ADMIN',
   }
 
   beforeEach(async () => {
@@ -35,17 +47,90 @@ describe('UserService', () => {
     expect(model).toBeDefined();
   });
 
-  /* describe('addUser', () => {
-    it('create a new user model and save it, returning the generated id', async () => {
-
+  describe('getAllUsers', () => {
+    it('should call find on the user model and return a result', async () => {
+      jest
+        .spyOn(model, 'find')
+        .mockImplementation(() => ({
+          exec: jest.fn().mockResolvedValue([mockUser])
+        } as any));
+      const result = await service.getAllUsers();
+      expect(model.find).toHaveBeenCalled();
+      expect(result).toEqual([mockUser]);
     });
-  }); */
+  });
 
-  //describe('getAllUsers', () => {});
+  describe('getUserById', () => {
+    beforeEach(() => {
+      jest
+        .spyOn(model, 'findById')
+        .mockImplementation(() => ({
+          exec: jest.fn().mockResolvedValue(mockUser)
+        } as any));
+    });
+    it('should call findById on the model and return a result', async () => {
+      const result = await service.getUserById(mockUser.id);
+      expect(model.findById).toHaveBeenCalledWith(mockUser.id);
+      expect(result).toEqual(mockUser);
+    });
+    it('should return a Http Exception when model returns null', async () => {
+      jest
+        .spyOn(model, 'findById')
+        .mockReturnValue({
+          exec: jest.fn().mockResolvedValueOnce(null),
+        } as any);
+      await expect(service.getUserById(mockUser.id)).rejects.toThrow(HttpException);
+      expect(model.findById).toHaveBeenCalledWith(mockUser.id);
+    });
+  });
 
-  //describe('getUserById', () => {});
+  describe('getUserByEmail', () => {
+    beforeEach(() => {
+      jest
+        .spyOn(model, 'findOne')
+        .mockImplementation(() => ({
+          exec: jest.fn().mockResolvedValue(mockUser)
+        } as any));
+    });
+    it('should call findOne on the model and return a result', async () => {
+      const result = await service.getUserByEmail(mockUser.email);
+      expect(model.findOne).toHaveBeenCalledWith({ 'email' : mockUser.email });
+      expect(result).toEqual(mockUser);
+    });
+    it('should return a Http Exception when model returns null', async () => {
+      jest
+        .spyOn(model, 'findOne')
+        .mockReturnValue({
+          exec: jest.fn().mockResolvedValueOnce(null),
+        } as any);
+      await expect(service.getUserByEmail(mockUser.email)).rejects.toThrow(HttpException);
+      expect(model.findOne).toHaveBeenCalledWith({ 'email' : mockUser.email });
+    });
+  });
 
-  //describe('getUserByEmail', () => {});
-
-  //describe('updateUser', () => {});
+  describe('updateUser', () => {
+    it('should call findByIdAndUpdate on the model and return a result', async () => {
+      const updatedUser = {
+        ...mockUser,
+        firstName: 'Updated',
+        lastName: 'Name'
+      };
+      jest
+        .spyOn(model, 'findByIdAndUpdate')
+        .mockImplementation(() => ({
+          exec: jest.fn().mockResolvedValue(updatedUser)
+        } as any));
+      const result = await service.updateUser(mockUser.id, updatedUser as any);
+      expect(model.findByIdAndUpdate).toHaveBeenCalledWith(
+        mockUser.id,
+        updatedUser,
+        {
+          new: true,
+          runValidators: true
+        }
+      );
+      expect(result.firstName).toEqual(updatedUser.firstName);
+      expect(result.lastName).toEqual(updatedUser.lastName);
+    });
+  });
 });
--- src/user/services/user/user.service.ts
+++ src/user/services/user/user.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, HttpException, Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from 'src/user/schemas/user.schema';
+import { User } from '../../schemas/user.schema';
 
 @Injectable()
 export class UserService {
@@ -50,7 +50,7 @@ export class UserService {
     this.logger.log(`Getting User with email: ${email}`, this.SERVICE);
     let user: User;
     try {
-      user = await this.userModel.findOne({ email: email });
+      user = await this.userModel.findOne({ email: email }).exec();
       this.logger.log(`Found user: ${user}`, this.SERVICE);
     } catch (error) {
       this.logger.error(`User not found, error message: ${error.message}`, this.SERVICE);
