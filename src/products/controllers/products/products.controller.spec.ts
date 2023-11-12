5252571df68cd3c6847fd4eddf95ee77268f46e1
CourtneyHoppus <cobaltab@gmail.com>
2023-11-12 09:49:52 -0800

    updated controller tests

--- src/products/controllers/products/products.controller.spec.ts
+++ src/products/controllers/products/products.controller.spec.ts
@@ -2,20 +2,22 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsController } from './products.controller';
 import { ProductsService } from '../../services/products/products.service';
 import { CreateProductDto } from '../../dto/create-product.dto';
-import { Response } from 'express';
+import { Product } from '../../schemas/product.schema';
+import { UpdateProductDto } from '../../dto/update-product.dto';
 
 
 describe('ProductsController', () => {
   let controller: ProductsController;
   let service: ProductsService;
-  let mockResponse: Response;
 
   let mockService = {
     createProduct: jest.fn(),
-    findAll: jest.fn()
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    updateProduct: jest.fn()
   }
 
-  let mockProduct = {
+  let mockCreateProductDto = {
     createByUserID: '',
     productType: [],
     gender: [],
@@ -26,6 +28,31 @@ describe('ProductsController', () => {
     productDescriptionOptional: 'string',
     productImage: 'string',
   } as unknown as CreateProductDto
+
+  let mockUpdateProductDto = {
+    createByUserID: '',
+    productType: [],
+    gender: [],
+    productShoeSize: [],
+    productSize: [],
+    productSizePantsWaist: [],
+    productSizePantsInseam: [],
+    productDescriptionOptional: 'string',
+    productImage: 'string',
+  } as unknown as UpdateProductDto
+
+  let mockProduct = {
+    id: 'idString',
+    createByUserID: '',
+    productType: [],
+    gender: [],
+    productShoeSize: [],
+    productSize: [],
+    productSizePantsWaist: [],
+    productSizePantsInseam: [],
+    productDescriptionOptional: 'string',
+    productImage: 'string',
+  } as unknown as Product
 
 
   beforeEach(async () => {
@@ -52,30 +79,49 @@ describe('ProductsController', () => {
   });
 
   describe('createProduct', () => {
-    // this passes as we are only expecting the controller to call the service
-    // this catches an error as there is no mock database set up for the mock service
     it(
-      'should call the createProduct method of the service and throw an error',
+      'should call createProduct on the service and return a response',
       async () => {
-        let response = await controller.createProduct(mockProduct, mockResponse);
-        //can add values to properties and expect it to fail
-        /* expect(service.createProduct).toHaveBeenCalledWith({
-          createByUserID: '',
-          productType: [],
-          gender: [],
-          productShoeSize: [],
-          productSize: [],
-          productSizePantsWaist: [],
-          productSizePantsInseam: [],
-          productDescriptionOptional: 'string',
-          productImage: 'string',
-        }); */
-        //can run this test with a different string and expect it to fail
-        //expect(response.message).toEqual("Failed to create product");
-        expect(service.createProduct).toHaveBeenCalledWith(mockProduct);
-        expect(response.message).toEqual("Failed to create product, please try again");
+        jest
+          .spyOn(service, 'createProduct')
+          .mockResolvedValue(mockProduct);
+        let response = await controller.createProduct(mockCreateProductDto);
+        expect(service.createProduct).toHaveBeenCalledWith(mockCreateProductDto);
+        expect(response).toEqual(mockProduct);
       }
     );
+  });
+
+  describe('getProducts', () => {
+    it('should call findAll on the service and return a result', async () => {
+      jest
+        .spyOn(service, 'findAll')
+        .mockResolvedValue([mockProduct]);
+      const result = await controller.getProducts();
+      expect(service.findAll).toHaveBeenCalled();
+      expect(result).toEqual([mockProduct]);
+    });
+  });
+
+  describe('findByID', () => {
+    it('should call findOne on the service and return a result', async () => {
+      jest
+        .spyOn(service, 'findOne')
+        .mockResolvedValue(mockProduct);
+      const result = await controller.findByID(mockProduct.id);
+      expect(service.findOne).toHaveBeenCalledWith(mockProduct.id);
+      expect(result).toEqual(mockProduct);
+    });
+  });
+
+  describe('updateProductById', () => {
+    it('should call updateProduct on the service and return a result', async () => {
+      jest
+        .spyOn(service, 'updateProduct')
+        .mockResolvedValue(mockProduct);
+      const result = await controller.updateProductById(mockProduct.id, mockUpdateProductDto);
+      expect(result).toEqual(mockProduct);
+    });
   });
 
 });
--- src/products/controllers/products/products.controller.ts
+++ src/products/controllers/products/products.controller.ts
@@ -2,11 +2,9 @@ import {
   Body,
   Controller,
   Get,
-  HttpStatus,
   Logger,
   Param,
   Post,
-  Res,
   Put,
   UseGuards,
   Inject,
@@ -17,6 +15,7 @@ import { UpdateProductDto } from '../../dto/update-product.dto';
 import { JwtAuthGuard } from '../../../auth/jwt.guard';
 import { Roles } from '../../../auth/roles.decorator';
 import { RoleGuard } from '../../../auth/role.guard';
+import { Product } from '../../schemas/product.schema';
 
 @Controller('products')
 export class ProductsController {
@@ -29,84 +28,32 @@ export class ProductsController {
   @Roles('admin')
   @UseGuards(JwtAuthGuard, RoleGuard)
   @Post('new')
-  async createProduct(@Body() productDto: CreateProductDto, @Res() response) {
-    this.logger.log(`Creating Product ${JSON.stringify(productDto, null, '\t')}`, this.CONTROLLER)
-    try {
-      const newProduct = await this.productService.createProduct(productDto);
-      return response.status(HttpStatus.CREATED).json({
-        message: 'Product successfully created',
-        product: newProduct,
-      });
-    } catch (error) {
-      this.logger.error(`Product not created, error message: ${error.message}`, this.CONTROLLER);
-      return {
-        message: "Failed to create product, please try again",
-        error: error.message,
-      };
-    }
+  async createProduct(@Body() productDto: CreateProductDto) {
+    this.logger.log(`Creating Product ${JSON.stringify(productDto, null, '\t')}`, this.CONTROLLER);
+    return await this.productService.createProduct(productDto as Product);
   }
   
   @Get('')
-  async getProducts(@Res() response) {
+  async getProducts() {
     this.logger.log('Finding all Products', this.CONTROLLER);
-    try {
-      const products = await this.productService.findAll();
-      return response.status(HttpStatus.OK).json({
-          message: 'Products successfully found',
-          products: products,
-      });
-    } catch (error) {
-      this.logger.error(`Products not found, error message: ${error.message}`, this.CONTROLLER);
-      return response.status(HttpStatus.NOT_FOUND).json({
-        message: "Products not found, please try again",
-        error: error.message,
-      });
-    }
+    return await this.productService.findAll();
   }
 
   @Get('find/:id')
-  async findByID(@Res() response, @Param('id') id: string) {
+  async findByID(@Param('id') id: string) {
     this.logger.log(`Finding Product with id: ${id}`, this.CONTROLLER);
-    try {
-      const product = await this.productService.findOne(id);
-      return response.status(HttpStatus.OK).json({
-        message: 'Product successfully found',
-        product: product,
-      });
-    } catch (error) {
-      this.logger.error(`Product not found, error message: ${error.message}`, this.CONTROLLER);
-      return response.status(HttpStatus.NOT_FOUND).json({
-        message: "Product not found, please try again",
-        error: error.message,
-      });
-    }
+    return await this.productService.findOne(id);
   }
 
   @Roles('admin')
   @UseGuards(JwtAuthGuard, RoleGuard)
   @Put('update/:id')
-  async updateProductById(
-    @Res() response,
-    @Param('id') id: string,
-    @Body() productDto: UpdateProductDto,
-  ) {
+  async updateProductById(@Param('id') id: string, @Body() productDto: UpdateProductDto) {
     this.logger.log(
       `Updating Product with id: ${id} with: ${JSON.stringify(productDto, null, '\t')}`, 
       this.CONTROLLER
     );
-    try {
-      const updatedProduct = await this.productService.updateProduct(id, productDto);
-      return response.status(HttpStatus.OK).json({
-        message: 'Product successfully updated',
-        product: updatedProduct,
-      });
-    } catch (error) {
-      this.logger.error(`Product not updated, error message: ${error.message}`, this.CONTROLLER);
-      return response.status(HttpStatus.NOT_FOUND).json({
-        message: "Failed to update product, please try again",
-        error: error.message,
-      });
-    }
+    return await this.productService.updateProduct(id, productDto as Product);
   }
 }
   
