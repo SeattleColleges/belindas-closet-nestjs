767fbe7fd4bde8869db26751f95d072429adb1a3
Intisar Osman <intisar.Osman@seattlecolleges.edu>
2023-11-13 07:00:35 -0800

    Refactored soft delete button
    Refactored soft delete button products.controller.ts in accordance with the rest of the code (also resolves any merge conflict with Courtney's PR)

--- src/products/controllers/products/products.controller.ts
+++ src/products/controllers/products/products.controller.ts
@@ -2,11 +2,9 @@ import {
   Body,
   Controller,
   Get,
-  HttpStatus,
   Logger,
   Param,
   Post,
-  Res,
   Put,
   UseGuards,
   Delete,
@@ -29,105 +27,41 @@ export class ProductsController {
   @Roles('admin')
   @UseGuards(JwtAuthGuard, RoleGuard)
   @Post('new')
-  async createProduct(@Body() productDto: CreateProductDto, @Res() response) {
-    this.logger.log(`Creating Product ${JSON.stringify(productDto, null, '\t')}`, this.CONTROLLER)
-    try {
-      const newProduct = await this.productService.createProduct(productDto);
-      return response.status(HttpStatus.CREATED).json({
-        message: 'Product successfully created',
-        product: newProduct,
-      });
-    } catch (error) {
-      this.logger.error(`Product not created, error message: ${error.message}`, this.CONTROLLER);
-      return {
-        message: "Failed to create product, please try again",
-        error: error.message,
-      };
-    }
+  async createProduct(@Body() productDto: CreateProductDto) {
+    this.logger.log(`Creating Product ${JSON.stringify(productDto, null, '\t')}`, this.CONTROLLER);
+    return await this.productService.createProduct(productDto);
   }
   
   @Get('')
-  async getProducts(@Res() response) {
+  async getProducts() {
     this.logger.log('Finding all Products', this.CONTROLLER);
-    try {
-      const products = await this.productService.findAll();
-      return response.status(HttpStatus.OK).json({
-          message: 'Products successfully found',
-          products: products,
-      });
-    } catch (error) {
-      this.logger.error(`Products not found, error message: ${error.message}`, this.CONTROLLER);
-      return response.status(HttpStatus.NOT_FOUND).json({
-        message: "Products not found, please try again",
-        error: error.message,
-      });
-    }
+    return await this.productService.findAll();
   }
 
   @Get('find/:id')
-  async findByID(@Res() response, @Param('id') id: string) {
+  async findByID(@Param('id') id: string) {
     this.logger.log(`Finding Product with id: ${id}`, this.CONTROLLER);
-    try {
-      const product = await this.productService.findOne(id);
-      return response.status(HttpStatus.OK).json({
-        message: 'Product successfully found',
-        product: product,
-      });
-    } catch (error) {
-      this.logger.error(`Product not found, error message: ${error.message}`, this.CONTROLLER);
-      return response.status(HttpStatus.NOT_FOUND).json({
-        message: "Product not found, please try again",
-        error: error.message,
-      });
-    }
+    return await this.productService.findOne(id);
   }
 
   @Roles('admin')
   @UseGuards(JwtAuthGuard, RoleGuard)
   @Put('update/:id')
-  async updateProductById(
-    @Res() response,
-    @Param('id') id: string,
-    @Body() productDto: UpdateProductDto,
-  ) {
+  async updateProductById(@Param('id') id: string, @Body() productDto: UpdateProductDto) {
     this.logger.log(
       `Updating Product with id: ${id} with: ${JSON.stringify(productDto, null, '\t')}`, 
       this.CONTROLLER
     );
-    try {
-      const updatedProduct = await this.productService.updateProduct(id, productDto);
-      return response.status(HttpStatus.OK).json({
-        message: 'Product successfully updated',
-        product: updatedProduct,
-      });
-    } catch (error) {
-      this.logger.error(`Product not updated, error message: ${error.message}`, this.CONTROLLER);
-      return response.status(HttpStatus.NOT_FOUND).json({
-        message: "Failed to update product, please try again",
-        error: error.message,
-      });
-    }
+    return await this.productService.updateProduct(id, productDto);
   }
-// }
-  
-  // we will more than likely change this delete function to an update sort of like a soft-delete. ex. isDeleted = true
+
+  // Soft Delete Button
 
    @Roles('admin')
    @UseGuards(JwtAuthGuard, RoleGuard)
     @Delete('remove/:id')
-    async deleteProduct(@Res() response, @Param('id') id: string) {
-      try {
-        const deletedProduct = await this.productService.delete(id);
-        return response.status(HttpStatus.OK).json({
-          message: 'Product successfully deleted',
-          product: deletedProduct,
-        });
-      } catch (error) {
-        return response.status(HttpStatus.NOT_FOUND).json({
-          message: "Failed to delete product, please try again",
-          error: error.message,
-        });
-      }
+    async deleteProduct(@Param('id') id: string) {
+        return await this.productService.delete(id);
     }
   }
 
