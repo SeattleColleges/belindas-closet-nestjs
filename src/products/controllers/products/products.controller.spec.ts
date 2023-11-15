7a891315d36c7b763dc84cf4a68de54df3d87ef6
CourtneyHoppus <74206103+CourtneyHoppus@users.noreply.github.com>
2023-11-14 19:30:30 -0800

    Belinda's-Closet-NestJS_3_112_Unit-Tests-Product-Controller-and-Service (#44)
    * injected service into controller
    
    * created service alias, provided this to test
    
    * test: controller calls service for createProduct()
    
    * added testing
    
    * added testing like nsc events
    
    * updated service tests
    
    * updated controller tests
    
    * added controller tests
    
    * added logging to both, .exec to delete in service
    
    * updated testing to include delete

--- src/products/controllers/products/products.controller.spec.ts
+++ src/products/controllers/products/products.controller.spec.ts
@@ -1,18 +1,140 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsController } from './products.controller';
+import { ProductsService } from '../../services/products/products.service';
+import { CreateProductDto } from '../../dto/create-product.dto';
+import { Product } from '../../schemas/product.schema';
+import { UpdateProductDto } from '../../dto/update-product.dto';
+
 
 describe('ProductsController', () => {
   let controller: ProductsController;
+  let service: ProductsService;
+
+  let mockService = {
+    createProduct: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    updateProduct: jest.fn(),
+    delete: jest.fn()
+  }
+
+  let mockCreateProductDto = {
+    createByUserID: '',
+    productType: [],
+    gender: [],
+    productShoeSize: [],
+    productSize: [],
+    productSizePantsWaist: [],
+    productSizePantsInseam: [],
+    productDescriptionOptional: 'string',
+    productImage: 'string',
+  } as unknown as CreateProductDto
+
+  let mockUpdateProductDto = {
+    createByUserID: '',
+    productType: [],
+    gender: [],
+    productShoeSize: [],
+    productSize: [],
+    productSizePantsWaist: [],
+    productSizePantsInseam: [],
+    productDescriptionOptional: 'string',
+    productImage: 'string',
+  } as unknown as UpdateProductDto
+
+  let mockProduct = {
+    id: 'idString',
+    createByUserID: '',
+    productType: [],
+    gender: [],
+    productShoeSize: [],
+    productSize: [],
+    productSizePantsWaist: [],
+    productSizePantsInseam: [],
+    productDescriptionOptional: 'string',
+    productImage: 'string',
+  } as Product
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
+      providers: [
+        {
+          provide: 'PRODUCTS_SERVICE',
+          useValue: mockService
+        }
+      ],
     }).compile();
 
     controller = module.get<ProductsController>(ProductsController);
+    service = module.get<ProductsService>('PRODUCTS_SERVICE');
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+  
+  it('service should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('createProduct', () => {
+    it(
+      'should call createProduct on the service and return a response',
+      async () => {
+        jest
+          .spyOn(service, 'createProduct')
+          .mockResolvedValue(mockProduct);
+        let response = await controller.createProduct(mockCreateProductDto);
+        expect(service.createProduct).toHaveBeenCalledWith(mockCreateProductDto);
+        expect(response).toEqual(mockProduct);
+      }
+    );
+  });
+
+  describe('getProducts', () => {
+    it('should call findAll on the service and return a result', async () => {
+      jest
+        .spyOn(service, 'findAll')
+        .mockResolvedValue([mockProduct]);
+      const result = await controller.getProducts();
+      expect(service.findAll).toHaveBeenCalled();
+      expect(result).toEqual([mockProduct]);
+    });
+  });
+
+  describe('findByID', () => {
+    it('should call findOne on the service and return a result', async () => {
+      jest
+        .spyOn(service, 'findOne')
+        .mockResolvedValue(mockProduct);
+      const result = await controller.findByID(mockProduct.id);
+      expect(service.findOne).toHaveBeenCalledWith(mockProduct.id);
+      expect(result).toEqual(mockProduct);
+    });
+  });
+
+  describe('updateProductById', () => {
+    it('should call updateProduct on the service and return a result', async () => {
+      jest
+        .spyOn(service, 'updateProduct')
+        .mockResolvedValue(mockProduct);
+      const result = await controller.updateProductById(mockProduct.id, mockUpdateProductDto);
+      expect(service.updateProduct).toHaveBeenCalledWith(mockProduct.id, mockUpdateProductDto);
+      expect(result).toEqual(mockProduct);
+    });
+  });
+
+  describe('deleteProduct', () => {
+    it('should call delete on the service and return a result', async () => {
+      jest
+        .spyOn(service, 'delete')
+        .mockResolvedValue(mockProduct);
+      const result = await controller.deleteProduct(mockProduct.id);
+      expect(service.delete).toHaveBeenCalledWith(mockProduct.id);
+      expect(result).toEqual(mockProduct);
+    });
+  });
+
 });
--- src/products/services/products/products.service.spec.ts
+++ src/products/services/products/products.service.spec.ts
@@ -1,18 +1,165 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
+import { Model } from 'mongoose';
+import { Product } from '../../schemas/product.schema';
+import { getModelToken } from '@nestjs/mongoose';
+import { NotFoundException } from '@nestjs/common';
 
 describe('ProductsService', () => {
   let service: ProductsService;
+  let model: Model<Product>;
+
+  let mockProductModel = {
+    create: jest.fn(),
+    find: jest.fn(),
+    findById: jest.fn(),
+    findByIdAndUpdate: jest.fn(),
+  }
+
+  let mockProduct = {
+    id: '123',
+    createByUserID: '',
+    productType: [],
+    gender: [],
+    productShoeSize: [],
+    productSize: [],
+    productSizePantsWaist: [],
+    productSizePantsInseam: [],
+    productDescriptionOptional: 'string',
+    productImage: 'string',
+    isHidden: false
+  } as Product
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        {
+          provide: getModelToken(Product.name),
+          useValue: mockProductModel,
+        }
+      ],
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
+    model = module.get<Model<Product>>(getModelToken(Product.name));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('model should be defined', () => {
+    expect(model).toBeDefined();
+  });
+
+  describe('createProduct', () => {
+    it('should call create on the model and return a product', async () => {
+      const newProduct = mockProduct;
+      jest
+        .spyOn(model, 'create')
+        .mockImplementationOnce(() => (Promise.resolve(newProduct) as any));
+      const result = await service.createProduct(newProduct as any);
+      expect(result).toEqual(newProduct);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should call find on the product model and return a result', async () => {
+      jest
+        .spyOn(model, 'find')
+        .mockImplementation(() => ({
+          exec: jest.fn().mockResolvedValue([mockProduct]),
+        } as any));
+      const result = await service.findAll();
+      expect(model.find).toHaveBeenCalled();
+      expect(result).toEqual([mockProduct]);
+    });
+    it('should return a Not Found Exception when model returns null', async () => {
+      jest
+        .spyOn(model, 'find')
+        .mockReturnValue({
+          exec: jest.fn().mockResolvedValueOnce(null),
+        } as any);
+      await expect(service.findAll()).rejects.toThrow(NotFoundException);
+      expect(model.find).toHaveBeenCalled();
+    });
+    it('should return a Not Found Exception when model returns an empty array', async () => {
+      jest
+        .spyOn(model, 'find')
+        .mockReturnValue({
+          exec: jest.fn().mockResolvedValueOnce([]),
+        } as any);
+      await expect(service.findAll()).rejects.toThrow(NotFoundException);
+      expect(model.find).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne', () => {
+    beforeEach(() => {
+      jest
+        .spyOn(model, 'findById')
+        .mockImplementation(() => ({
+          exec: jest.fn().mockResolvedValue(mockProduct),
+        } as any));
+    });
+    it('should call findById on the model and return a result', async () => {
+      const result = await service.findOne(mockProduct.id);
+      expect(model.findById).toHaveBeenCalledWith(mockProduct.id);
+      expect(result).toEqual(mockProduct);
+    });
+    it('should return a Not Found Exception when model returns null', async () => {
+      jest
+        .spyOn(model, 'findById')
+        .mockReturnValue({
+          exec: jest.fn().mockResolvedValueOnce(null),
+        } as any);
+      await expect(service.findOne(mockProduct.id)).rejects.toThrow(NotFoundException);
+      expect(model.findById).toHaveBeenCalledWith(mockProduct.id);
+    });
+  });
+
+  describe('updateProduct', () => {
+    it('should call findByIdAndUpdate on the model and return a result', async () => {
+      const updatedProduct = {
+        ...mockProduct,
+        productType: ['updated', 'product']
+      };
+      jest
+        .spyOn(model, 'findByIdAndUpdate')
+        .mockReturnValue({
+          exec: jest.fn().mockResolvedValueOnce(updatedProduct)
+        } as any);
+      const result = await service.updateProduct(mockProduct.id, updatedProduct as any);
+      expect(model.findByIdAndUpdate).toHaveBeenCalledWith(
+        mockProduct.id,
+        updatedProduct,
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+      expect(result).toEqual(updatedProduct);
+    });
+  });
+  
+  describe('delete', () => {
+    it('should call findByIdAndUpdate on the model and return a result', async () => {
+      const deletedProduct = {
+        ...mockProduct,
+        isHidden: true
+      };
+      jest
+        .spyOn(model, 'findByIdAndUpdate')
+        .mockReturnValue({
+          exec: jest.fn().mockResolvedValueOnce(deletedProduct)
+        } as any);
+      const result = await service.delete(mockProduct.id);
+      expect(model.findByIdAndUpdate).toHaveBeenCalledWith(
+        mockProduct.id,
+        { isHidden: true }
+      );
+      expect(result).toEqual(deletedProduct);
+    });
   });
 });
