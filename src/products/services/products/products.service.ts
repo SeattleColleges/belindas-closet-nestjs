06ff0197f93b2965bf6edb0eca5f81a2612e1c6e
CourtneyHoppus <cobaltab@gmail.com>
2023-12-06 14:54:21 -0800

    refactored update and find, updated unit tests

--- src/products/services/products/products.service.ts
+++ src/products/services/products/products.service.ts
@@ -38,10 +38,14 @@ export class ProductsService {
 
   async findOne(id: string): Promise<Product> {
     this.logger.log(`Finding Product with id: ${id}`, this.SERVICE);
+    if (!Types.ObjectId.isValid(id)) {
+      this.logger.warn('Invalid Id format', this.SERVICE);
+      throw new BadRequestException('Invalid Id format');
+    }
     const product = await this.productModel.findById(id).exec();
     if (!product || product === null) {
-      this.logger.warn('Product not found');
-      throw new NotFoundException(`Product ${id} not found`, this.SERVICE);
+      this.logger.warn('Product not found', this.SERVICE);
+      throw new NotFoundException(`Product ${id} not found`);
     }
     this.logger.log(
       `Product found: ${JSON.stringify(product, null, '\t')}`,
@@ -51,27 +55,30 @@ export class ProductsService {
   }
 
   async updateProduct(id: string, product: Product): Promise<Product> {
+    this.logger.log(`Updating Product with id: ${id}`, this.SERVICE);
+    if (!Types.ObjectId.isValid(id)) {
+      this.logger.warn('Invalid Id format', this.SERVICE);
+      throw new BadRequestException('Invalid Id format');
+    }
+    if (!product || product === null) {
+      this.logger.warn('Updated product not supplied', this.SERVICE);
+      throw new BadRequestException('Updated product not supplied');
+    }
     this.logger.log(
-      `Updating Product with id: ${id} with: ${JSON.stringify(
-        product,
-        null,
-        '\t',
-      )}`,
+      `Updated Product; ${JSON.stringify(product, null, '\t')}`,
       this.SERVICE,
     );
-    if (!product || product === null) {
-      // what is logged and the exception thrown is not correct
-      // this is checking if a product with updated properities is sent from the controller
-      // no new product was supplied, this is a Bad Request
-      this.logger.warn('Product not found');
-      throw new NotFoundException(`Product ${id} not found`);
-    }
-    return await this.productModel
+    const updatedProduct = await this.productModel
       .findByIdAndUpdate(id, product, {
         new: true,
         runValidators: true,
       })
       .exec();
+    if (!updatedProduct || updatedProduct === null) {
+      this.logger.warn('Product not found', this.SERVICE);
+      throw new NotFoundException(`Product ${id} not found`);
+    }
+    return updatedProduct;
   }
 
   async delete(id: string): Promise<Product> {
