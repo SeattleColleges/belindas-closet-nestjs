3caa67593302908dce347ff6f68cbbaac733907d
Tin Pham <nhattin55@gmail.com>
2024-03-06 23:49:21 -0800

    implemented change password API

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { HttpException, Injectable } from '@nestjs/common';
-import { InjectModel, getModelToken } from '@nestjs/mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/user/schemas/user.schema';
 import { SignUpDto } from '../dto/signup.dto';
