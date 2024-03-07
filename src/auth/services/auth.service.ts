601616c4a5a1c3be7c5a90f8df93ff17430c9246
kaimanasse15 <91504156+kaimanasse15@users.noreply.github.com>
2024-03-07 14:34:38 -0800

    SES for nextjs

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,127 +1,56 @@
-import { HttpException, Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { User } from 'src/user/schemas/user.schema';
-import { SignUpDto } from '../dto/signup.dto';
-import { LoginDto } from '../dto/login.dto';
-import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
-import { ForgotPasswordDto } from '../dto/forgot-password.dto';
-import { MailService } from '@sendgrid/mail';
+// Load the AWS SDK for Node.js
+var AWS = require("aws-sdk");
+// Set the region
+AWS.config.update({ region: "REGION" });
 
-@Injectable()
-export class AuthService {
-  constructor(
-    @InjectModel(User.name)
-    private userModel: Model<User>,
-    private jwtService: JwtService,
-  ) {}
-
-  async signUp(signUpDto: SignUpDto) {
-    const { firstName, lastName, email, password, role } = signUpDto;
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await this.userModel.findOne({ email });
-    if (user) {
-      throw new HttpException('Email already exists', 400);
-    }
-    const newUser = await this.userModel.create({
-      firstName,
-      lastName,
-      email,
-      password: hashedPassword,
-      role,
-    });
-
-    const token = this.jwtService.sign({
-      message: 'Sign up successfully',
-      id: newUser._id,
-      firstName: newUser.firstName,
-      lastName: newUser.lastName,
-      email: newUser.email,
-      role: newUser.role,
-    });
-
-    return { token };
-  }
-
-  async login(loginDto: LoginDto) {
-    const { email, password } = loginDto;
-    const user = await this.userModel.findOne({ email });
-    if (!user) {
-      throw new HttpException('Invalid credentials', 400);
-    }
-
-    // compare password with hashed password
-    const hashedPassword = user.password;
-    const isValidPassword = await bcrypt.compare(password, hashedPassword);
-
-    if (!isValidPassword) {
-      throw new HttpException('Invalid credentials', 400);
-    }
-
-    const token = this.jwtService.sign({
-      message: 'Login successful',
-      id: user._id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      email: user.email,
-      role: user.role,
-    });
-
-    return { token };
-  }
-  // generate product key for user to access the product
-  generateProductKey(email: string, role: string) {
-    const key = `${email}-${role}-${process.env.PRODUCT_KEY}`;
-    const hashedKey = bcrypt.hashSync(key, 10);
-    return hashedKey;
-  }
-
-  // forgot password function
-  async forgotPassword(forgotPasswordDto: ForgotPasswordDto) {
-    const { email } = forgotPasswordDto;
-
-    // check if user exists
-    const user = await this.userModel.findOne({ email });
-
-    if (!user) {
-      throw new HttpException('User does not exist', 404);
-    }
-
-    // generate new password
-    const newPassword = Math.random().toString(36).slice(-8);
-
-    // update user password
-    const hashedPassword = await bcrypt.hash(newPassword, 10);
-    await this.userModel.updateOne(
-      {
-        email,
+// Create sendEmail params
+var params = {
+  Destination: {
+    /* required */
+    CcAddresses: [
+      "EMAIL_ADDRESS",
+      /* more items */
+    ],
+    ToAddresses: [
+      "EMAIL_ADDRESS",
+      /* more items */
+    ],
+  },
+  Message: {
+    /* required */
+    Body: {
+      /* required */
+      Html: {
+        Charset: "UTF-8",
+        Data: "HTML_FORMAT_BODY",
       },
-      {
-        password: hashedPassword,
+      Text: {
+        Charset: "UTF-8",
+        Data: "TEXT_FORMAT_BODY",
       },
-    );
-    // send new password to user
-    const mailService = new MailService();
-    mailService.setApiKey(process.env.SENDGRID_API_KEY);
+    },
+    Subject: {
+      Charset: "UTF-8",
+      Data: "Test email",
+    },
+  },
+  Source: "SENDER_EMAIL_ADDRESS" /* required */,
+  ReplyToAddresses: [
+    "EMAIL_ADDRESS",
+    /* more items */
+  ],
+};
 
-    const msg = {
-      to: email, // For testing purposes, replace with your email to see the email
-      from: process.env.SENDER_EMAIL,
-      subject: 'Reset Password',
-      html: `<p>Your new password is <strong>${newPassword}</strong>. <br> Please change your password after logging in</p>`,
-    };
+// Create the promise and SES service object
+var sendPromise = new AWS.SES({ apiVersion: "2010-12-01" })
+  .sendEmail(params)
+  .promise();
 
-    mailService
-      .send(msg)
-      .then(() => {
-        console.log('Email sent');
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-
-    return { message: 'Password reset successfully' };
-  }
-}
+// Handle promise's fulfilled/rejected states
+sendPromise
+  .then(function (data) {
+    console.log(data.MessageId);
+  })
+  .catch(function (err) {
+    console.error(err, err.stack);
+  });
