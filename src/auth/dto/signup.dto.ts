57186ec9691f4b464cd49d837fad4647b891bef9
Isaac J <36749051+IsaacJrTypes@users.noreply.github.com>
2024-05-02 12:22:45 -0700

    Belindas closet nestjs 5 101 qa testing (#109)
    * add file changes
    
    * renames test file
    
    * add testing configurations

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,25 +1,25 @@
-import { Role } from "src/user/schemas/user.schema";
-import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+import { Role } from '../../user/schemas/user.schema';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class SignUpDto {
-    @IsNotEmpty()
-    @IsString()
-    readonly firstName: string;
+  @IsNotEmpty()
+  @IsString()
+  readonly firstName: string;
 
-    @IsNotEmpty()
-    @IsString()
-    readonly lastName: string;
+  @IsNotEmpty()
+  @IsString()
+  readonly lastName: string;
 
-    @IsNotEmpty()
-    @IsEmail({}, { message: 'Please enter a valid email' })
-    readonly email: string;
+  @IsNotEmpty()
+  @IsEmail({}, { message: 'Please enter a valid email' })
+  readonly email: string;
 
-    @IsNotEmpty()
-    @IsString()
-    @MinLength(8, { message: 'Password must be at least 8 characters long' })
-    readonly password: string;
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  readonly password: string;
 
-    @IsNotEmpty()
-    @IsString()
-    readonly role: Role;
+  @IsNotEmpty()
+  @IsString()
+  readonly role: Role;
 }
