d8975864bae88e389de876673ea87d30923a22df
alasali1 <alasali96@gmail.com>
2024-06-06 22:25:27 -0700

    attempt to remove unrelated field

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmpty, IsOptional, IsString, IsBoolean, IsEnum } from "class-validator";
+import { ValidateIf, IsEmpty, IsOptional, IsString, IsBoolean, IsEnum } from "class-validator";
 import { User } from "../../user/schemas/user.schema";
 
 export class CreateProductDto {
@@ -12,18 +12,22 @@ export class CreateProductDto {
     @IsEnum(['MALE','FEMALE', 'NON_BINARY'])
     readonly productGender: [];
   
+    @ValidateIf((object, value) => object.productType === 'Shoes')
     @IsOptional()
     @IsEnum([null, 5, 6, 7, 8, 9, 10, 11, 12])
     readonly productSizeShoe: [];
   
+    @ValidateIf((object, value) => ['Shirts', 'Skirts', 'Jacket/Blazer',  'Dress', 'Casual Wear', 'Suits'].includes(object.productType))
     @IsOptional()
     @IsEnum(['SELECT_SIZE', 'XS', 'S', 'M', 'L', 'XL', 'XXL'])
     readonly productSizes: [];
   
+    @ValidateIf((object, value) => object.productType === 'Pants')
     @IsOptional()
     @IsEnum([null, 28, 30, 32, 34, 36, 38, 40, 42])
     readonly productSizePantsWaist: []; // Assuming size is a number
   
+    @ValidateIf((object, value) => object.productType === 'Pants')
     @IsOptional()
     @IsEnum([null, 28, 30, 32, 34, 36, 38, 40, 42])
     readonly productSizePantsInseam: []; // Assuming size is a number
