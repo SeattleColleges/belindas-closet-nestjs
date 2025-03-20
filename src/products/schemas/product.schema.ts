de4fc4ebca6a0d14fba2473adf43bebf6e1ad665
Keiffer <keiffer213@gmail.com>
2025-03-20 11:15:35 -0700

    Update product.schema.ts
    update prop types so it is just strings and allows optional props

--- src/products/schemas/product.schema.ts
+++ src/products/schemas/product.schema.ts
@@ -1,54 +1,45 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import mongoose, { Document } from "mongoose";
 import { User } from "../../user/schemas/user.schema";
+import {ProductType, ProductGender, ProductSizes, ShoeSize, PantsSize} from "../enums"
 
-@Schema({
-    timestamps: true,
-    collection: 'products'
-})
-
+@Schema({ timestamps: true, collection: "products" })
 
 export class Product extends Document {
-    @Prop ({
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-    })
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: "User", required: true })
     createdByUser: User;
-
-    @Prop ({
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-    })
+  
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: "User" })
     updatedByUser: User;
-
-    @Prop()
-    productType: [];
-
-    @Prop()
-    productGender: [];
-
-    @Prop()
-    productSizeShoe: [];
-
-    @Prop()
-    productSizes: [];
-
-    @Prop()
-    productSizePantsWaist: [];
-
-    @Prop()
-    productSizePantsInseam: [];
-
-    @Prop()
-    productDescription: string;
-
-    @Prop()
-    productImage: string;
-
-    @Prop({ default: false })
+  
+    @Prop({ type: String, enum: ProductType, required: true })
+    productType: ProductType;
+  
+    @Prop({ type: String, enum: ProductGender, required: false })
+    productGender?: ProductGender;
+  
+    @Prop({ type: Number, enum: ShoeSize, required: false, default: null })
+    productSizeShoe?: ShoeSize | null;
+  
+    @Prop({ type: String, enum: ProductSizes, required: false, default: null })
+    productSizes?: ProductSizes | null;
+  
+    @Prop({ type: Number, enum: PantsSize, required: false, default: null })
+    productSizePantsWaist?: PantsSize | null;
+  
+    @Prop({ type: Number, enum: PantsSize, required: false, default: null })
+    productSizePantsInseam?: PantsSize | null;
+  
+    @Prop({ type: String, required: false })
+    productDescription?: string;
+  
+    @Prop({ type: String, required: false })
+    productImage?: string;
+  
+    @Prop({ type: Boolean, default: false })
     isHidden: boolean;
-
-    @Prop({ default: false })
+  
+    @Prop({ type: Boolean, default: false })
     isSold: boolean;
 }
 
