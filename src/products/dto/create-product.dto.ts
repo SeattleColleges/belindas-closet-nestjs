154e582c8647955d80b64b015c6af9ea36feea0d
Keiffer <keiffer213@gmail.com>
2025-03-20 11:17:12 -0700

    Update create-product.dto.ts
    update the "crea-product" dto to match new enum and data type as well as optional

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,46 +1,44 @@
 import { IsEmpty, IsOptional, IsString, IsBoolean, IsEnum } from "class-validator";
 import { User } from "../../user/schemas/user.schema";
+import {ProductType, ProductGender, ProductSizes, ShoeSize, PantsSize} from "../enums"
 
 export class CreateProductDto {
     @IsEmpty({message: 'ID field is not required'})
-    readonly createdByUser: User;
+    createdByUser: User;
     
-    @IsEnum(['Shoes', 'Shirts', 'Pants', 'Skirts', 'Jacket/Blazer', 'Dress', 'Casual Wear', 'Suits', 'Accessories'])
-    readonly productType: [];
+    @IsEnum(ProductType)
+    productType: ProductType;
   
-    @IsOptional()
-    @IsEnum(['MALE','FEMALE', 'NON_BINARY'])
-    readonly productGender: [];
+    @IsEnum(ProductGender)
+    productGender: ProductGender;
   
+    // @IsEnum(ShoeSize)
     @IsOptional()
-    @IsEnum([null, 5, 6, 7, 8, 9, 10, 11, 12])
-    readonly productSizeShoe: [];
+    productSizeShoe?: null | ShoeSize;
   
+    // @IsEnum(ProductSizes)
     @IsOptional()
-    @IsEnum(['SELECT_SIZE', 'XS', 'S', 'M', 'L', 'XL', 'XXL'])
-    readonly productSizes: [];
+    productSizes?: ProductSizes | null;
   
+    // @IsEnum(PantsSize)
     @IsOptional()
-    @IsEnum([null, 28, 30, 32, 34, 36, 38, 40, 42])
-    readonly productSizePantsWaist: []; // Assuming size is a number
+    productSizePantsWaist?: PantsSize | null;
   
+    // @IsEnum(PantsSize)
     @IsOptional()
-    @IsEnum([null, 28, 30, 32, 34, 36, 38, 40, 42])
-    readonly productSizePantsInseam: []; // Assuming size is a number
-  
-    @IsOptional()
-    @IsString()
-    readonly productDescription: string;
+    productSizePantsInseam?: PantsSize | null;
   
     @IsOptional()
     @IsString()
-    readonly productImage: string;
+    productDescription?: string;
   
+    @IsString()
     @IsOptional()
-    @IsBoolean()
-    readonly isHidden: boolean;
+    productImage?: string;
   
-    @IsOptional()
     @IsBoolean()
-    readonly isSold: boolean;
+    isHidden: boolean;
+  
+    @IsBoolean()
+    isSold: boolean;
 }
